850311d8db06240202f84cac00a9da46414caedb
Geethacsc <85781907+Geethacsc@users.noreply.github.com>
2021-06-13 18:10:42 +0530

    day 1 task

---
+++ day2challengeoutput.py
@@ -0,0 +1,26 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: C:\Python 3.9\assignment day2.py ==================
+DAY 2 CHALLENGE
+30 days 30 hour challenge
+30 days 30 hour challenge
+thirty
+thirty
+T
+win
+ilw
+ill win
+I will
+Ii
+I will win
+10
+i will win
+30 Days30 hours
+30 Days 30 hours
+thirty days and thirty hours
+Thirty days and thirty hours
+8
+False
+False
+>>> 
