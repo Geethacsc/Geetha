809750132aa028d256c4a1b068382732e879a440
Geethacsc <85781907+Geethacsc@users.noreply.github.com>
2021-06-18 18:03:18 +0530

    day 5 task

---
+++ day5output.py
@@ -0,0 +1,48 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: C:\Python 3.9\day5task.py ======================
+enter the number to insert:@132
+new list: ['2', 'lord', '45', 'jg143', '45', '@132']
+enter the number to insert:zeebra
+new list: ['2', 'lord', '45', 'jg143', '45', '@132', 'zeebra']
+enter the number to insert:1008
+new list: ['2', 'lord', '45', 'jg143', '45', '@132', 'zeebra', '1008']
+enter the choice:1
+list after insertion: ['2', 'lord', '45', 'jissajesus', 'jg143', '45', '@132', 'zeebra', '1008']
+do u want to continue:yes
+o?yes
+enter the choice:2
+list after deletion: ['2', 'jissajesus', 'jg143', '45', '@132', 'zeebra', '1008']
+do u want to continue:yes
+o?yes
+enter the choice:3
+list after extention: ['2', 'jissajesus', 'jg143', '45', '@132', 'zeebra', '1008', 'ahteeg', 'susej', 'alijiv', 341]
+do u want to continue:yes
+o?yes
+enter the choice:4
+list after using pop function: ['2', 'jissajesus', 'jg143', '45', '@132', 'zeebra', '1008', 'ahteeg', 'susej', 'alijiv']
+do u want to continue:yes
+o?yes
+enter the choice:5
+enter the number to remove:45
+new list: ['2', 'jissajesus', 'jg143', '@132', 'zeebra', '1008', 'ahteeg', 'susej', 'alijiv']
+do u want to continue:yes
+o?yes
+enter the choice:5
+enter the number to remove:1
+given value is not in the list
+do u want to continue:yes
+o?yes
+enter the choice:d
+WRONG CHOICE!
+
+do u want to continue:yes
+o?no
+____end of the loop____
+maximum variable in the list: zeebra
+minimum variable in the list: 1008
+tuple: ('vg', '674', '45', '@yh', 'ahteeg')
+Reverse of tuple : ('ahteeg', '@yh', '45', '674', 'vg')
+['456', 'tihr', 'tiger', '#@bh']
+>>> 
