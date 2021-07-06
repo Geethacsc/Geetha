f04c6f8a0fb078f96cbb5a8933a239bf17dcd92f
Geethacsc <85781907+Geethacsc@users.noreply.github.com>
2021-07-06 09:01:44 +0530

    Add files via upload

---
+++ task10output.py
@@ -0,0 +1,19 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================= RESTART: C:/Python 3.9/task10.py =======================
+		_____________TASK 10_____________
+Enter Account no: XC34517643
+Enter name: Jissa
+d-deposit,w-withdrawl
+Enter choice:d
+Enter amount to be deposited:3400000
+Balance amount: 3400000.0
+Do you want to continue?yes/no:yes
+d-deposit,w-withdrawl
+Enter choice:w
+Enter amount to be withdrawn:390000
+Balance amount: 3010000.0
+Do you want to continue?yes/no:no
+		_______End______ 
+>>> 
